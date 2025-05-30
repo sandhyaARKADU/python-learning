2f8d9c6b5535b9936f2a1bc44d32d5e6b7ce3849
sandhyaARKADU <sandhayareddy03507@gmail.com>
2025-05-30 13:53:24 +0530

    initial changes

---
+++ apps.py
@@ -0,0 +1,9 @@
+temperature = 35
+if temperature > 30:
+    print("It's warm")
+    print("Dronk water")
+elif temperature > 20:
+    print("It's nice")
+else:
+    print("It's cold")
+    print("Done")
---
+++ python projects/number_guessing_game.py
@@ -0,0 +1,18 @@
+import random
+number_to_guess = random.randint(1, 100)
+while True:
+  try:
+    guess = int(input('Guess the number between 1 and 100:'))
+
+    if guess < number_to_guess:
+      print('Too low!')
+
+    elif guess > number_to_guess:
+      print('Too high!')
+    else:
+      print('Congratulation! You guesse the number.')
+      break
+
+
+  except ValueError:
+    print('Please enter a valid number')
